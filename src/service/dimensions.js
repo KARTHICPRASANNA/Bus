b83b26a7073eab7e8c4bcb42fc6ec5919c5aa7ec
KARTHICPRASANNA <you@example.com>
2022-12-05 23:55:56 +0530

    Moved data to service file.

---
+++ src/service/dimensions.js
@@ -0,0 +1,171 @@
+/* eslint-disable max-statements */
+/* eslint-disable max-lines-per-function */
+/* eslint-disable no-magic-numbers */
+import { range } from '@laufire/utils/collection';
+import { rndValue } from '@laufire/utils/random';
+
+const colors = ['CadetBlue', 'firebrick', 'paleVioletRed'];
+
+const outerTyreMultiplier = 1000;
+const OuterTyreNumber = 2;
+const OuterTyreMargin = 245;
+
+const rimMultiplier = 1000;
+const rimNumber = 2;
+const rimMargin = 272;
+
+const doorMultiplier = 108;
+const doorCount = 2;
+const doorMargin = 530;
+
+const windowMultiplier = 350;
+const windowCount = 4;
+const windowMargin = 430;
+
+const luggageDoorMultiplier = 231;
+const luggageDoorCount = 2;
+const luggageDoorMargin = 785;
+
+const luggageHandleMultiplier = 230;
+const luggageHandleCount = 2;
+const luggageHandleMargin = 890;
+
+const getBusBody = (size) => ({
+	top: 100 * size,
+	left: 150 * size,
+	width: 1700 * size,
+	height: 600 * size,
+	busBodyColor: rndValue(colors),
+});
+
+const getDoor = (size) => ({
+	top: 122 * size,
+	left: 510 * size,
+	width: 250 * size,
+	height: 450 * size,
+});
+
+const getDoorHandle = (size) => ({
+	top: 305 * size,
+	left: 629 * size,
+	width: 20 * size,
+	height: 50 * size,
+});
+
+const getRoof = (size) => ({
+	top: -30 * size,
+	left: 115 * size,
+	width: 1550 * size,
+	height: 20 * size,
+});
+
+const getSticker = (size) => ({
+	top: 290 * size,
+	left: 770 * size,
+	width: 925 * size,
+	height: 0,
+});
+
+const getLuggageFrame = (size) => ({
+	top: 344 * size,
+	left: 780 * size,
+	width: 460 * size,
+	height: 230 * size,
+});
+
+const getLuggageSection = (size) => ({
+	top: 344 * size,
+	left: 1010 * size,
+	width: 0,
+	height: 230 * size,
+});
+
+const getFrontGlass = (size) => ({
+	top: 0,
+	left: 0,
+	width: 105 * size,
+	height: 215 * size,
+});
+
+const getRedLight = (size) => ({
+	top: 360 * size,
+	left: 1700 * size,
+	width: 18 * size,
+	height: 50 * size,
+});
+
+const getRearIndicator = (size) => ({
+	top: 414 * size,
+	left: 1700 * size,
+	width: 18 * size,
+	height: 50 * size,
+});
+
+const getOuterTyres = (size) => range(0, OuterTyreNumber).map((tyre) => ({
+	top: 490 * size,
+	left: ((tyre * outerTyreMultiplier) + OuterTyreMargin) * size,
+	width: 200 * size,
+	height: 200 * size,
+}));
+
+const getRims = (size) => range(0, rimNumber).map((tyre) => ({
+	top: 509 * size,
+	left: ((tyre * rimMultiplier) + rimMargin) * size,
+	width: 135 * size,
+	height: 150 * size,
+}));
+
+const getDoorInnerFrames = (size) => range(0, doorCount).map((door) => ({
+	top: 145 * size,
+	left: ((door * doorMultiplier) + doorMargin) * size,
+	width: 100 * size,
+	height: 400 * size,
+}));
+
+const getWindows = (size) => range(1, windowCount).map((window) => ({
+	top: 30 * size,
+	left: ((window * windowMultiplier) + windowMargin) * size,
+	width: 180 * size,
+	height: 180 * size,
+}));
+
+const getLuggageDoors = (size) => range(0, luggageDoorCount).map((door) => ({
+	top: 567 * size,
+	left: ((door * luggageDoorMultiplier) + luggageDoorMargin) * size,
+	width: 220 * size,
+	height: 0,
+}));
+
+const getLuggageDoorsHandles = (size) =>
+	range(0, luggageHandleCount).map((handle) => ({
+		top: 564 * size,
+		left: ((handle * luggageHandleMultiplier)
+	+ luggageHandleMargin) * size,
+		width: 30 * size,
+		height: 10 * size,
+	}));
+
+const getDimension = (size) => {
+	const busBody = getBusBody(size);
+	const door = getDoor(size);
+	const doorHandle = getDoorHandle(size);
+	const roof = getRoof(size);
+	const sticker = getSticker(size);
+	const luggageFrame = getLuggageFrame(size);
+	const luggageSection = getLuggageSection(size);
+	const frontGlass = getFrontGlass(size);
+	const redLight = getRedLight(size);
+	const rearIndicator = getRearIndicator(size);
+	const outerTyres = getOuterTyres(size);
+	const rims = getRims(size);
+	const doorInnerFrames = getDoorInnerFrames(size);
+	const windows = getWindows(size);
+	const luggageDoors = getLuggageDoors(size);
+	const luggageDoorsHandles = getLuggageDoorsHandles(size);
+
+	return { busBody, door, doorHandle, roof, sticker, luggageFrame,
+		luggageSection, frontGlass, redLight, rearIndicator, outerTyres,
+		rims, doorInnerFrames, windows, luggageDoors, luggageDoorsHandles };
+};
+
+export default getDimension;
